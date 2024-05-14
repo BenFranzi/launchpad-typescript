910e68715290ddf1b8626d3a6ffcdc79420f14ba
Ben Franzi <me@benfranzi.com>
2024-05-14 12:22:07 +1000

    initalise project

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {browser: true, es2020: true},
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@bengineer.dev/core'
+  ],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "error",
+    "object-curly-spacing": ["error", "always"],
+    "no-unused-vars": "off",
+    "padding-line-between-statements": "off",
+    "import/no-nodejs-modules": "off"
+  },
+  // TEST CONDITIONS - REMOVE FOR REAL PROJECTS
+  overrides: [
+    {
+      files: ['*'],
+      rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "prefer-arrow-callback": "off",
+        "no-console": "warn",
+        "import/unambiguous": "off"
+      }
+    },
+    //\TEST CONDITIONS - REMOVE FOR REAL PROJECTS
+  ],
+}
